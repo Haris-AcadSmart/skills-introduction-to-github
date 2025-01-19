b19370fa484e6e93810793c1919d83e721a2dcbc
Haris-AcadSmart <harissa766076@gmail.com>
2025-01-19 15:40:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+
+// Function to handle product item clicks and show modals
+const productItems = document.querySelectorAll('.product-item');
+const modal = document.getElementById("myModal");
+const modalContent = document.querySelector
